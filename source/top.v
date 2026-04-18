131463c8208ea4077d331f1746081cc010eb0c25
Aidan <jah202@duke.edu>
2026-04-18 18:40:55 -0400

    creating top file

---
+++ source/top.v
@@ -0,0 +1,18 @@
+/*
+Top module for connect 4 game
+
+Manages:
+CPU:
+imem (compiled assembly): "connectFour.mem"
+dmem:
+servo: "ServoController.v"
+
+Notes:
+Register Mapped Input (must add code to make CPU see hardware as a register)
+
+*/
+module top
+
+
+
+endmodule
