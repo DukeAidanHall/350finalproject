d9f38eaf50e3994b3a8b2d7f4f5e356fedb377dd
Aidan <jah202@duke.edu>
2026-04-22 04:14:59 -0400

    Implement AI control and modify button logic
    Added AI functionality and adjusted button handling.

--- source/top.v
+++ source/top.v
@@ -37,6 +37,7 @@ module top (
     input rightButtonExt, //exterior right press 
     input dropButtonExt, //exterior drop press 
     input resetButtonExt, //exterior reset press
+    //input aiButtonExt, //exterior ai press (TRIAL)
     
     output[14:0] LED, // debug LEDs
     output servoSignal, // servo 1 (left/right)
@@ -51,18 +52,22 @@ module top (
     reg [31:0] BTNDPress = 0;
     reg oldBTNC = 0;
     reg [31:0] BTNCPress = 0;
+    reg aiEnabled = 0;
+    reg player2Turn = 0;
 
     wire [31:0] column;
+    
+    reg [31:0] aiDelay = 32'd0;
 
     wire Err;
     wire Win1;
     wire Win2;
     assign LED[14] = Err;
-    assign LED[11] = Win1;
+    assign LED[11] = aiDelay[0];
     assign player1win = Win1;
-    assign LED[12] = Win2;
+    assign LED[12] = player2Turn;
     assign player2win = Win2;
-    assign LED[13] = BTNDPress;
+    assign LED[13] = aiEnabled;
 
     always @(posedge clk) begin //Builds the medium speed clock
         if(counter < 32'd50000000) begin
@@ -77,16 +82,31 @@ module top (
         oldBTND <= resetButtonExt;
         if(resetButtonExt && !(oldBTND)) begin //BTND press
             BTNDPress <= 32'b1;
+            aiEnabled <= 1'b0;
         end
         else begin
             BTNDPress <= 32'b0;
         end
-        oldBTNC <= (dropButtonExt);
-        if(dropButtonExt && !(oldBTNC)) begin //BTNC press
-            BTNCPress <= 32'b1;
+        oldBTNC <= (dropButtonExt2);
+        if(dropButtonExt2 && !(oldBTNC)) begin //BTNC press
+            BTNCPress2 <= 32'b1;
+            if(player2Turn == 0) begin
+                player2Turn <= 1'b1;
+            end
+            else begin
+                player2Turn <= 1'b0;
+            end
         end
         else begin
-            BTNCPress <= 32'b0;
+            BTNCPress2 <= 32'b0;
+        end
+        if(BTND) begin
+            if(aiEnabled == 0) begin
+                aiEnabled <= 1'b1;
+            end
+            else begin
+                aiEnabled <= 1'b0;
+            end
         end
     end
     
@@ -95,13 +115,147 @@ module top (
     
     wire [8:0] LED80;
     assign LED[8:0] = LED80;
+    
+    //Control Params for AI
+    reg BTNDPress_02 = 0;
+    reg [31:0] BTNCPress2 = 0;
+    reg [31:0] BTNDPress2 = 0;
+    reg dropButtonExt2 = 0;
+    reg rightButtonExt2 = 0;
+    reg leftButtonExt2 = 0;
 
-    Wrapper CPUWrapper(.clock(clk), .reset(BTNDPress[0]), .BTNC_In(BTNCPress), 
-    .BTND_In(BTNDPress), .Column_In(column_adjusted), .Err_out(Err), 
+    reg [31:0] initialColumn = 0;
+    reg [31:0] aiDelayCounter = 0;
+
+    always @(posedge clk) begin 
+        if(player2Turn && aiEnabled) begin
+            if(aiDelayCounter < 32'd100000000) begin
+                aiDelayCounter <= aiDelayCounter+1;
+            end else begin
+                aiDelayCounter <= 0;
+                aiDelay <= aiDelay + 1;
+            end
+            if(aiDelay<10) begin
+                initialColumn <= column;
+
+                BTNDPress_02 <= BTNDPress[0];
+                BTNDPress2 <= BTNDPress;
+                dropButtonExt2 <= 0;
+                rightButtonExt2 <= 0;
+                leftButtonExt2 <= 0;
+            end
+            else if (aiDelay<15) begin
+                if(initialColumn == 0) begin
+                    rightButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 1) begin
+                    leftButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 2) begin
+                    rightButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 3) begin
+                    rightButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 4) begin
+                    leftButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 5) begin
+                    rightButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 6) begin
+                    leftButtonExt2 <= 1'b1;
+                end
+            end
+            else if(aiDelay <20) begin
+                if(initialColumn == 0) begin
+                    rightButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 1) begin
+                    leftButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 2) begin
+                    rightButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 3) begin
+                    rightButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 4) begin
+                    leftButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 5) begin
+                    rightButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 6) begin
+                    leftButtonExt2 <= 1'b0;
+                end
+            end
+
+            else if(aiDelay < 35) begin
+                if(initialColumn == 0) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 1) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 2) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 3) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 4) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 5) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+                else if(initialColumn == 6) begin
+                    dropButtonExt2 <= 1'b1;
+                end
+            end
+
+            else if(aiDelay < 40) begin
+
+                if(initialColumn == 0) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 1) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 2) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 3) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 4) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 5) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+                else if(initialColumn == 6) begin
+                    dropButtonExt2 <= 1'b0;
+                end
+            end
+
+        end else begin
+            aiDelay <= 32'd0;
+            BTNDPress_02 <= BTNDPress[0];
+            BTNDPress2 <= BTNDPress;
+            dropButtonExt2 <= dropButtonExt;
+            rightButtonExt2 <= rightButtonExt;
+            leftButtonExt2 <= leftButtonExt;
+        end
+    end
+
+    Wrapper CPUWrapper(.clock(clk), .reset(BTNDPress_02), .BTNC_In(BTNCPress2), 
+    .BTND_In(BTNDPress2), .Column_In(column_adjusted), .Err_out(Err), 
     .Win1_out(Win1), .Win2_out(Win2));
 
-    ServoController Servo(.clk(clk), .BTNC_In(dropButtonExt), .BTNR_In(rightButtonExt), 
-    .BTNL_In(leftButtonExt), .LED_Out(LED80), .servoSignal_Out(servoSignal),
+    ServoController Servo(.clk(clk), .BTNC_In(dropButtonExt2), .BTNR_In(rightButtonExt2), 
+    .BTNL_In(leftButtonExt2), .LED_Out(LED80), .servoSignal_Out(servoSignal),
     .servoSignal2_Out(servoSignal2), .currentColumn(column));
 
 endmodule
